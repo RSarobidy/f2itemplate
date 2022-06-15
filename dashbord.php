13be763443e14c859da94d779dbb435d289346e8
AllanBUSI <busi.travail@gmail.com>
2022-06-15 11:37:21 +0200

    feat(bdd) add bdd

--- dashbord.php
+++ dashbord.php
@@ -1,4 +1,5 @@
 <?php 
+require_once 'Database.php';
 
 //  traite une donnée
 
@@ -10,7 +11,7 @@ $phone = htmlspecialchars($_POST['phone']);
 $mdp1 = htmlspecialchars($_POST['mdp1']);
 $mdp2 = htmlspecialchars($_POST['mdp2']);
 
-// 
+
 
 // if == si 
 // empty == vide 
@@ -28,21 +29,44 @@ if (strlen($nom) == 0 || strlen($nom) > 255 OR empty($nom)) {
 
 if (strlen($prenom) == 0 || strlen($prenom) > 255 OR empty($prenom)) {
   echo "erreur prenom";
+  die();
 }
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL) || strlen($date) > 255){
   echo $email." mail invalide". "<br>";
+  die();
   
 }
 
 if (strlen($phone) == 0 || strlen($phone) >18 OR empty($phone)) {
   echo "numero invalide";
+  die();
 }
 
 if(empty($date) || strlen($date) > 20) {
   echo "erreur date";
+  die();
 }
 
 if(strlen($mdp1) < 12 || $mdp1 != $mdp2 || strlen($mdp1) > 255 ) {
   echo "erreur mdp";
+  die();
 }
+
+$rechercheEmail = Database::Select('user', 'email = ?', [$email]);
+
+if ($rechercheEmail == false) {
+  echo "erreur de vérification";
+  die();
+}
+
+$email = $rechercheEmail->fetchAll();
+
+if (count($email) > 0) {
+  echo "L'utilisateur est déjà inscrit";
+  die();
+}
+
+$newPassword = password_hash($mdp1, PASSWORD_ARGON2ID);
+
+var_dump($newPassword);
